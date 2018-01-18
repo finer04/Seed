dbd2ab4057f3039dc221686f2389d327da8fa98e
finer04 <popkart7175@qq.com>
2018-01-18 14:39:14 +0800

    fix feature image
    修复无图文章的特色图片问题

--- functions.php
+++ functions.php
@@ -23,11 +23,12 @@ function timer_stop( $display = 0, $precision = 3 ) {
 function showThumbnail($widget) {
 $attach = $widget->attachments(1)->attachment;
 $pattern = '/\<img.*?src\=\"(.*?)\"[^>]*>/i';
+$random = 'https://finer04-cos.b0.upaiyun.com/yourname.jpg';
 if (preg_match_all($pattern, $widget->content, $thumbUrl)) {
 echo $thumbUrl[1][0];
 } else
-if ($attach->isImage) {
+if ($attach && $attach->isImage) {
 echo $attach->url;
 } else {
-echo $random;
+echo $random ;
 } }
